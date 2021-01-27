dad50efcc368c9fdfbf0709861407792413fd4d7
OlegBiletsky <ol.biletsky@gmail.com>
2021-01-27 15:58:02 +0200

    added js for scrolling

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ let ham = document.getElementById("hamburger-button");
 function toggleHamburger(){
     navbar.classList.toggle("showNav");
     ham.classList.toggle("showClose");
+    ToggleScroll();
 }
 
 ham.addEventListener("click", toggleHamburger);
@@ -15,3 +16,22 @@ menuLinks.forEach(
     menuLink.addEventListener("click", toggleHamburger) 
   }
 )
+
+
+
+
+//JS for disabling scroll in mobile  
+
+// select body
+let bodyElement = document.body;
+// select menu toggler
+let togglerElement = document.getElementById('toggler');
+
+// function which toggle overflow for body
+function ToggleScroll() {
+    if (bodyElement.style.overflow === "hidden") {
+            bodyElement.style.overflow = "auto"// scroll ON
+    } else {
+        bodyElement.style.overflow = "hidden"//scroll OFF
+    }
+}
